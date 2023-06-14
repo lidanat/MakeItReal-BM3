46433d32fa6fe2b9d97226b6854b0319221a9ecd
Lidana Taborda <lidanay@gmail.com>
2023-06-14 18:55:30 -0500

    add conditionals and numbers

---
+++ JavaScript/conditionals/numbers.js
@@ -0,0 +1,17 @@
+let firstNum = 23;
+let secondNum = 23;
+
+if (firstNum < secondNum) {
+  console.log("The number " + firstNum + " is less than " + secondNum);
+}
+
+else
+{
+    if(firstNum > secondNum){
+        console.log("The number " + firstNum + " is greater than " + secondNum);
+    }
+    else {
+        console.log("The number " + firstNum + " is equal to " + secondNum);
+    }
+    
+}
--- JavaScript/variables/square.js
+++ JavaScript/variables/square.js
@@ -1,4 +1,4 @@
 let side = 8;
 
-console.log("The perimeter of a square of " + side + " is " + (5 * 4));
-console.log("The area of a square of " + side + " is " + (5 * 5));
+console.log("The perimeter of a square of " + side + " is " + (side * 4));
+console.log("The area of a square of " + side + " is " + (side * side));
