9e110c551b67bd09e13c01b4a0ef8235d5dc4743
Lidana Taborda <lidanay@gmail.com>
2023-06-20 22:27:20 -0500

    add console log - to the entire array

--- JavaScript/mission7/runningpositions.js
+++ JavaScript/mission7/runningpositions.js
@@ -6,4 +6,8 @@ let newArray = ["Fifth", "Sixth"];
 arr.splice(4, 0, newArray[0])
 arr.splice(5, 0, newArray[1])
 
-console.log('Final Array: ', arr)
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
+
+
