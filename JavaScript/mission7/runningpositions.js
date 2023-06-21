e13bb253ec574a31ae3ed2a7a07ecd96b2e1e147
Lidana Taborda <lidanay@gmail.com>
2023-06-20 22:16:50 -0500

    add mission 7

---
+++ JavaScript/mission7/runningpositions.js
@@ -0,0 +1,8 @@
+let arr= ["First", "Second", "Third", "Fourth", "I am the last Element"];
+console.log('Initial Array: ' , arr);
+
+let newArray = ["Fifth", "Sixth"];
+//Inserting positions in the array
+arr.splice(4, 0, newArray[0])
+arr.splice(5, 0, newArray[1])
+console.log('Final Array: ', arr)
---
+++ JavaScript/mission7/sumfunction.js
@@ -0,0 +1,11 @@
+//Sum of 2 arguments
+function sumArguments(firstNumber, secondNumber){
+    return firstNumber + secondNumber;
+}
+
+//let sumResult = sumArguments(10, 10);
+
+console.log('The sum is ' + sumArguments(10, 20))
+console.log('The sum is ' + sumArguments(10, 10))
+console.log('The sum is ' + sumArguments(40, 60))
+console.log('The sum is ' + sumArguments(50, 200))
