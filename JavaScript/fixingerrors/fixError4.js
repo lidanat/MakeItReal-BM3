4eb13487147110113b864dc4aabd3b8f8ecd7067
Lidana Taborda <lidanay@gmail.com>
2023-06-26 19:39:28 -0500

    Add Html folder and properties

--- JavaScript/fixingerrors/fixError4.js
+++ JavaScript/fixingerrors/fixError4.js
@@ -6,7 +6,7 @@ function getVoteCount(obj) {
 
 console.log('Result Case 1: ' + getVoteCount({upvotes:13, downvotes:0}))
 console.log('Result Case 1: ' +getVoteCount({upvotes:2, downvotes:33}))
-console.log('Result Case 1: ' +getVoteCount({upvotes:132, downvotes:132}) )
+console.log('Result Case 1: ' +getVoteCount({upvotes:132, downvotes:132}))
 
 
     
